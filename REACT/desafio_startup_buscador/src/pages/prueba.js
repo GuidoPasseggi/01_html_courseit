38d41813be8789b3429dd867e22425576d1664dc
Chorizard <guido.passeggi@gmail.com>
2020-07-16 20:27:23 -0300

    startups: added ROUTE prueba.js

--- REACT/desafio_startup_buscador/src/pages/prueba.js
+++ REACT/desafio_startup_buscador/src/pages/prueba.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
 
 class Prueba extends React.Component {
-  render(){
-    return(
-      <p>Soy una prueba</p>
-      
-      <Link to="/">Ir a la home</Link>
-    )
+  render() {
+    return (
+      <React.Fragment>
+        <p>Soy una prueba</p>
+
+        <Link to="/">Ir a la home</Link>
+        <Footer />
+      </React.Fragment>
+    );
   }
 }
 
 export default Prueba;
+
+//// vamos a necesitar:
+// 7 onChange
+// 1 onclick + ifs en el onClick y JSX
+// estado interno con cada input
+// estados que se modifiquen con cada onChange
+// validaciones
